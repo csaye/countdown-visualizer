55f5a6a0ab20896f56f764d0e8e6ea5b3f3cb116
Cooper Saye <coopersaye@gmail.com>
2021-10-31 16:13:55 -0700

    Clean up files.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+
+export default function App(props) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Countdown Visualizer</title>
+        <meta name="description" content="Visualize your next deadline." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp
