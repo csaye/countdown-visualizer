797cf2707352762254026684371a713538c42bad
Cooper Saye <coopersaye@gmail.com>
2021-11-01 23:06:59 -0700

    Import favicon.

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,9 @@ export default function App(props) {
       <Head>
         <title>Countdown Visualizer</title>
         <meta name="description" content="Visualize your next deadline." />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
