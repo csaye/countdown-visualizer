e567a49eee4edde177140f2431086867298ffb05
Cooper Saye <coopersaye@gmail.com>
2021-11-01 17:23:54 -0700

    Implement datetime input.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
+import { useState } from 'react';
+
 import styles from '../styles/Index.module.css';
 
 export default function Index() {
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+
   return (
     <div>
+      <input
+        type="date"
+        value={date}
+        onChange={e => setDate(e.target.value)}
+      />
+      <input
+        type="time"
+        value={time}
+        onChange={e => setTime(e.target.value)}
+      />
+      <h1>{date} {time}</h1>
     </div>
   );
 }
